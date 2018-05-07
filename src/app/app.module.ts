f98482165f989d5efecbe7a27f7ae3729bdcdb81
mouraoinfo <marcos@mourao.info>
2018-05-07 16:16:36 -0300

    compoonente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ConversorComponent } from './conversor/conversor.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConversorComponent
   ],
   imports: [
     BrowserModule
